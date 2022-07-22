0064b234bce9ee537cbb3ef4fa46ae38b313e747
unknown <shroukalkalla1997@gmail.com>
2022-07-22 15:30:42 +0200

    Fixed error messages

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import indexRoutes from './routes/index';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(indexRoutes);
+app.all('*', (req: Request, res: Response): Response => {
+   return res.status(404).json({
+      error: `This resource: ${req.originalUrl} does not exist`
+   });
+});
 
 app.listen(PORT, () => {
    console.log(`app is listening on http://localhost:${PORT}`);
--- src/middlewares/queryValidator.ts
+++ src/middlewares/queryValidator.ts
@@ -4,10 +4,17 @@ class QueryValidator {
    public static validate(req: Request, res: Response, next: NextFunction) {
       const width = req.query.width;
       const height = req.query.height;
-      if (width && height) {
-         if (!(Number(width) > 0) && !(Number(height) > 0)) {
+      const filename = req.query.filename;
+      if (!filename) {
+         return res.status(400).json({
+            error: 'You must enter a valid filename'
+         });
+      }
+
+      if (width || height) {
+         if (!(Number(width) > 0) || !(Number(height) > 0)) {
             return res.status(400).json({
-               error: 'Width And Height Must Be A Number'
+               error: 'Width And Height are required and must be a number'
             });
          }
       }
